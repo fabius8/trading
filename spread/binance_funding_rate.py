cc00cc0ddfd9f23f88af0d7e1f54d3b80bd29812
fabius8 <fabius8@163.com>
2020-02-13 09:25:41 +0800

    --wip--

--- spread/binance_funding_rate.py
+++ spread/binance_funding_rate.py
@@ -6,31 +6,38 @@ import ccxt
 import smtplib
 import os
 
-config = json.load(open('config.json'))
-Base = config["Base"]
-Quote = config["Quote"]
-Contract = config["Contract"]
-Spread_threshold = config["Spread_threshold"]
-Close_threshold = config["Close_threshold"]
-Min_MarginRatio = config["Min_MarginRatio"]
-Min_trade_amount = config["Min_trade_amount"]
-Trade_mode = config["trade_mode"]
+config = json.load(open('config_funding.json'))
 
-A = ccxt.binance(config["binance"])
+binance_future = ccxt.binance(config["binance_future"])
+binance_spot = ccxt.binance(config["binance_spot"])
+binance_future.load_markets()
+binance_spot.load_markets()
+
 old_fundingRate = None
+
+
+def get_spread(symbol, future, spot):
+    symbol = symbol.replace('USDT', '/USDT')
+    order_book_A = future.fetch_order_book(symbol)
+    bid0_A = order_book_A['bids'][0][0]
+    order_book_B = spot.fetch_order_book(symbol)
+    ask0_B = order_book_B['asks'][0][0]
+    return " %+.4f" % ((bid0_A - ask0_B)/ask0_B*100) + "%"
 
 
 while True:
     try:
         print("="*50)
         log = ""
-        fundingRate = A.fapiPublicGetPremiumIndex()
+        fundingRate = binance_future.fapiPublicGetPremiumIndex()
         print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime()))
         for i in fundingRate:
+            spread = get_spread(i['symbol'], binance_future, binance_spot)
             log = i['symbol'].ljust(8) + \
                   " fundingRate:" + \
                   " %.6f" % (float(i['lastFundingRate']) * 100)
             if old_fundingRate is None:
+                log += spread
                 print(log)
                 continue
             for j in old_fundingRate:
@@ -38,8 +45,10 @@ while True:
                     delta_fundingRate = float(i['lastFundingRate']) - \
                                         float(j['lastFundingRate'])
                     if delta_fundingRate == 0:
+                        log += spread
                         break
                     log += ' %+.6f' % (delta_fundingRate * 100)
+                    log += spread
                     break
             print(log)
         old_fundingRate = fundingRate
